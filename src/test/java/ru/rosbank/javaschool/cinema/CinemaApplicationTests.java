eb171acb293c00f9c49ad6381619328d51279c90
Eugene Solodimov <solodimov.eugene@yandex.ru>
2019-12-15 16:57:28 +0300

    Initial commit

---
+++ src/test/java/ru/rosbank/javaschool/cinema/CinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rosbank.javaschool.cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
