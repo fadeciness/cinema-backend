1debbf7cf995b8d482f9f0ef43417f924e4d7bad
Eugene Solodimov <solodimov.eugene@yandex.ru>
2019-12-20 12:43:37 +0300

    Finish

--- src/main/java/ru/rosbank/javaschool/cinema/service/FilmService.java
+++ src/main/java/ru/rosbank/javaschool/cinema/service/FilmService.java
@@ -2,20 +2,14 @@ package ru.rosbank.javaschool.cinema.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.rosbank.javaschool.cinema.constant.Names;
 import ru.rosbank.javaschool.cinema.dto.FilmDto;
 import ru.rosbank.javaschool.cinema.dto.FilmSaveRequestDto;
 import ru.rosbank.javaschool.cinema.entity.FilmEntity;
-import ru.rosbank.javaschool.cinema.enumeration.Genre;
 import ru.rosbank.javaschool.cinema.enumeration.Name;
 import ru.rosbank.javaschool.cinema.exception.FilmNotFoundException;
 import ru.rosbank.javaschool.cinema.repository.FilmRepository;
 
-import javax.annotation.PostConstruct;
 import javax.transaction.Transactional;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
@@ -29,53 +23,53 @@ public class FilmService {
     private final FilmRepository repository;
     private final Path path = Paths.get(String.valueOf(Name.UPLOAD_PATH));
 
-    @PostConstruct
-    public void init() throws IOException {
-        if (Files.notExists(path)) {
-            Files.createDirectories(path);
-        }
-        String picName = Names.GARFIELD_IMAGE;
-        byte[] picBytes = this.getClass().getClassLoader().getResourceAsStream(picName).readAllBytes();
-        String out = Names.UPLOAD_PATH + "/" + picName;
-        try (FileOutputStream fos = new FileOutputStream(out)) {
-            fos.write(picBytes);
-        }
-        String movName = Names.GARFIELD_VIDEO;
-        byte[] movBytes = this.getClass().getClassLoader().getResourceAsStream(movName).readAllBytes();
-        out = Names.UPLOAD_PATH + "/" + movName;
-        try (FileOutputStream fos = new FileOutputStream(out)) {
-            fos.write(movBytes);
-        }
-
-        FilmEntity first = new FilmEntity(
-                0,
-                "Гарфилд",
-                "Хозяин кота Гарфилда, Джон, приносит в дом милого щенка Оди, и с этого момента жизнь Гарфилда идёт наперекосяк...",
-                picName,
-                movName,
-                List.of(Genre.FAMILY, Genre.COMEDY, Genre.CARTOON),
-                null
-        );
-        repository.save(first);
-
-        picName = Names.INCEPTION_IMAGE;
-        picBytes = this.getClass().getClassLoader().getResourceAsStream(picName).readAllBytes();
-        out = Names.UPLOAD_PATH + "/" + picName;
-        try (FileOutputStream fos = new FileOutputStream(out)) {
-            fos.write(picBytes);
-        }
-
-        FilmEntity second = new FilmEntity(
-                0,
-                "Начало",
-                "Кобб — талантливый вор, лучший из лучших в опасном искусстве извлечения: он крадет ценные секреты из глубин подсознания во время сна, когда человеческий разум наиболее уязвим...",
-                picName,
-                null,
-                List.of(Genre.ACTION, Genre.DRAMA),
-                null
-        );
-        repository.save(second);
-    }
+//    @PostConstruct
+//    public void init() throws IOException {
+//        if (Files.notExists(path)) {
+//            Files.createDirectories(path);
+//        }
+//        String picName = Names.GARFIELD_IMAGE;
+//        byte[] picBytes = this.getClass().getClassLoader().getResourceAsStream(picName).readAllBytes();
+//        String out = Names.UPLOAD_PATH + "/" + picName;
+//        try (FileOutputStream fos = new FileOutputStream(out)) {
+//            fos.write(picBytes);
+//        }
+//        String movName = Names.GARFIELD_VIDEO;
+//        byte[] movBytes = this.getClass().getClassLoader().getResourceAsStream(movName).readAllBytes();
+//        out = Names.UPLOAD_PATH + "/" + movName;
+//        try (FileOutputStream fos = new FileOutputStream(out)) {
+//            fos.write(movBytes);
+//        }
+//
+//        FilmEntity first = new FilmEntity(
+//                0,
+//                "Гарфилд",
+//                "Хозяин кота Гарфилда, Джон, приносит в дом милого щенка Оди, и с этого момента жизнь Гарфилда идёт наперекосяк...",
+//                picName,
+//                movName,
+//                List.of(Genre.FAMILY, Genre.COMEDY, Genre.CARTOON),
+//                null
+//        );
+//        repository.save(first);
+//
+//        picName = Names.INCEPTION_IMAGE;
+//        picBytes = this.getClass().getClassLoader().getResourceAsStream(picName).readAllBytes();
+//        out = Names.UPLOAD_PATH + "/" + picName;
+//        try (FileOutputStream fos = new FileOutputStream(out)) {
+//            fos.write(picBytes);
+//        }
+//
+//        FilmEntity second = new FilmEntity(
+//                0,
+//                "Начало",
+//                "Кобб — талантливый вор, лучший из лучших в опасном искусстве извлечения: он крадет ценные секреты из глубин подсознания во время сна, когда человеческий разум наиболее уязвим...",
+//                picName,
+//                null,
+//                List.of(Genre.ACTION, Genre.DRAMA),
+//                null
+//        );
+//        repository.save(second);
+//    }
 
     public List<FilmDto> getAll() {
         return repository.findAll().stream()
